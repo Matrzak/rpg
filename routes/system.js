5fbbef99cdfe89b7bfbc4992c987315b39f51306
Matrzak <Matrzak@op.pl>
2019-11-11 22:23:55 +0100

    added name support

--- routes/system.js
+++ routes/system.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {ensureAuthenticated} = require('../auth/auth');
 
-router.get('/', ensureAuthenticated, (req, res) => res.render('dashboard'));
+router.get('/', ensureAuthenticated, (req, res) => res.render('dashboard', {nick: req.user.nick}));
 
 module.exports = router;
