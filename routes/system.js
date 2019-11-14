51bba44c65810b9b28ed8098a90b961216f2f270
Matrzak <Matrzak@op.pl>
2019-11-14 18:23:15 +0100

    new layout

--- routes/system.js
+++ routes/system.js
@@ -4,8 +4,30 @@ const {ensureAuthenticated} = require('../auth/auth');
 const Character = require("../models/Character");
 const ObjectId = require('mongoose').Types.ObjectId;
 
+function getRankAttributes(permissions){
+    let values = [];
+    switch (permissions) {
+        case 0:
+            values = ["Gracz", "secondary"];
+            break;
+        case 1:
+            values = ["Game Master", "success"];
+            break;
+        case 2:
+            values = ["Moderator", "info"];
+            break;
+        case 3:
+            values = ["Administrator", "danger"];
+            break;
+        default:
+            values = ["Gracz", "secondary"];
+            break;
+    }
+    return values;
+}
+
 router.get('/', ensureAuthenticated, (req, res) => {
-    res.render('dashboard', {nick: req.user.nick});
+    res.render('dashboard', {user: req.user, rankA: getRankAttributes(req.user.rank)});
 });
 router.get('/characters', ensureAuthenticated, (req,res) => {
     let characters = [];
@@ -29,5 +51,18 @@ router.get('/characters/info/*', ensureAuthenticated, (req,res) => {
         res.render('view', {character: c});
     });
 });
+
+router.get('/characters/create', ensureAuthenticated, (req,res) => {
+    res.render('create');
+});
+
+router.post('/characters/delete', ensureAuthenticated, (req,res) => {
+    const {value} = req.body;
+   console.log(value);
+});
+
+router.get('/newSystem', (req,res) => {
+    res.render('newPage');
+})
 
 module.exports = router;
